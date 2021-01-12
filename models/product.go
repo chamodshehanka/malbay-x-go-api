c2719b35a93b15ee9c78bef7b33f20679068b37d
Shehanka <hcsperera@outlook.com>
2021-01-12 17:48:55 +0530

    Added

--- models/product.go
+++ models/product.go
@@ -3,10 +3,10 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Product struct {
-	ID           primitive.ObjectID `bson:"_id,omitempty"`
-	Name         string             `bson:"name,omitempty"`
-	Description  string             `bson:"description,omitempty"`
-	Stocks       int                `bson:"stocks"`
-	Price        int                `bson:"price"`
-	ImageGallery string             `bson:"imageGallery"`
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	Name        string             `bson:"name,omitempty"`
+	Description string             `bson:"description,omitempty"`
+	Stocks      int                `bson:"stocks"`
+	Price       int                `bson:"price"`
+	Image       string             `bson:"image"`
 }
