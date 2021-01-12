6b0f5ec01aecb2a9a54f9ad1abf55f1bf8fe8fb5
Shehanka <hcsperera@outlook.com>
2021-01-12 16:06:53 +0530

    DB Config Added

--- go.mod
+++ go.mod
@@ -3,10 +3,14 @@ module github.com/Shehanka/malbay-x-go-api
 go 1.15
 
 replace github.com/Shehanka/malbay-x-go-api/api => ./api
+
 replace github.com/Shehanka/malbay-x-go-api/api/routes => ./api/routes
+
 replace github.com/Shehanka/malbay-x-go-api/api/handlers => ./api/handlers
 
 require (
 	github.com/go-chi/chi v1.5.1
 	github.com/go-chi/cors v1.1.1
+	github.com/spf13/viper v1.7.1
+	go.mongodb.org/mongo-driver v1.4.4
 )
--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"github.com/Shehanka/malbay-x-go-api/config"
+	"go.mongodb.org/mongo-driver/bson"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/Shehanka/malbay-x-go-api/api"
 )
 
 func main() {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client := config.GetMongoDBConnection()
+
+	testDB := client.Database("ShopDB")
+	podcastsCollection := testDB.Collection("admins")
+
+	cursor,err := podcastsCollection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	fmt.Println(cursor)
+
 	routes, err := api.LoadAPI()
 	if err != nil {
 		log.Fatal(err)
