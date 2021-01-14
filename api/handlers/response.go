ef225fbf8dc6d07b9be653b9424f4e4ad3547164
ebonynon <sithumsandeepap@gmail.com>
2021-01-14 11:51:26 +0530

    Add fun for respond err

---
+++ api/handlers/response.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// RespondWithError return error message
+func RespondWithError(w http.ResponseWriter, code int, msg interface{}) {
+
+	RespondwithJSON(w, code, map[string]interface{}{"message": msg})
+
+}
+
+// RespondwithJSON write json response format
+func RespondwithJSON(w http.ResponseWriter, code int, payload interface{}) {
+
+	t := "data"
+
+	if code > 300 {
+
+		t = "errors"
+
+	}
+
+	response, _ := json.Marshal(map[string]interface{}{
+		t: payload,
+	})
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
